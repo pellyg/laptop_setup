321ce3f9e46d5a8946b6081af3b22b2c0b5b06ce
Greg Pelly <pintadel@gmail.com>
2018-03-29 13:49:01 -0700

    add a bunch of files

---
+++ configure_laptop_settings.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# mac os x show dotfiles in finder
+defaults write com.apple.finder AppleShowAllFiles YES
+
+
+mkdir ~/screenshots
+defaults write com.apple.screencapture location ~/screenshots
